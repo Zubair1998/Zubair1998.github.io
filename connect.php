f0ccd557e01996a7e94fe421b03599b69f625778
ZubairMoahid <zubairbazark786@gmail.com>
2020-10-04 22:55:35 +0530

    update

--- connect.php
+++ connect.php
@@ -1,19 +1,16 @@
-<? php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
+<?php
+if(!empty($_POST["send"])) {
+	$name = $_POST["userName"];
+	$email = $_POST["userEmail"];
+	$subject = $_POST["subject"];
+	$content = $_POST["content"];
 
-$email_from = 'zubairbazark786@gmail.com';
-
-$email_subject = "New Form Submission";
-$email_body = "User Name: $name.\n".
-               "User Email: $visitor_email.\n".
-               "User Message: $message.\n";
-
-$to = "zubairmoahid786@gmail.com";
-$headers = "From: #email_from \r\n";
-$headers = "Reply-To: $visitor_email \r\n";
-mail($to,$email_subject,$email_body,$headers);
-header("Location: index.html")
-    
-    ?>
+	$toEmail = "zubairmoahid786@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "contact-view.php";
+?>
